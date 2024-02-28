0a2b30bcd54182f6e6c669e46b18ca2e94d4bf5e
Siddhant Kumar Keshri <siddhantkeshri2806@gmail.com>
2024-02-29 02:12:50 +0530

    404 page added

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import videoRouter from "./routes/channel.routes.js"
 import playlistRouter from "./routes/playlist.routes.js";
 import commentRouter from "./routes/coment.routes.js";
 import channelRouter from "./routes/channel.routes.js";
-
+import path from "path";
 
 const app=express();
 
@@ -22,6 +22,24 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"))
 app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const allowedRoutes = [
+    "/api/v1/users",
+    "/api/v1/channels",
+    "/api/v1/playlists",
+    "/api/v1/comments"
+];
+
+app.use((req, res, next) => {
+    if (!allowedRoutes.includes(req.path)) {
+        res.status(404).render('404', { allowedRoutes });
+    } else {
+        next();
+    }
+});
 
 app.use("/api/v1/users",userRouter);
 
